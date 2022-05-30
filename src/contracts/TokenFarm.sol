f5f10a8a1dba76bb3eea7a5d4884e3afb68a1364
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-05-30 13:03:15 +0100

    constructor

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,5 +1,14 @@
 pragma solidity >=0.5.0 <0.9.0;
+import "./DappToken.sol";
+import "./DaiToken.sol";
 
 contract TokenFarm {
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dapptoken, DaiToken _daiToken) public {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
